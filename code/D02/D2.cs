fcab7e4916cfd267d984533f423a158690d436ad
Daxin18 <kamilciag123@gmail.com>
2025-12-03 13:19:46 +0100

    D2p2

--- code/D02/D2.cs
+++ code/D02/D2.cs
@@ -9,6 +9,7 @@ public class D2 : IDay
 {
     List<(long from,long to)> ranges = new List<(long, long)>();
     long invalid_sum = 0;
+    bool use_alternate_invalid = true;
 
     public void Solve(){
         foreach ((long from, long to) range in ranges)
@@ -51,6 +52,9 @@ public class D2 : IDay
     {
         var stringified = id.ToString();
 
+        if (use_alternate_invalid)
+            return AlternateCheckInvalid(stringified);
+
         if (stringified.Length % 2 != 0)
             return false;
         
@@ -60,6 +64,56 @@ public class D2 : IDay
         var second = stringified.Substring(half);
 
         return first == second;
+    }
+
+    private bool AlternateCheckInvalid(string id)
+    {
+        if (id.Length < 2)
+            return false;
+
+        if (OneLongSeq(id))
+            return true;
+
+        if (id.Length < 4)
+            return false;
+
+        int half = id.Length / 2;
+
+        // i is the length of a sequence
+        for (int i = 2; i <= half; i++)
+        {
+            if (id.Length % i != 0) //no point in searching for i-length sequence if ID is not divisible by it
+                continue;
+            
+            var sequence = id.Substring(0, i); //get first sequence
+
+            bool do_continue = false;
+
+            for (int j = i; j < id.Length; j += i) //iterate over next sequences
+            {
+                if (id.Substring(j, i) != sequence) //if sequence differs from the first, move on to longer sequences
+                {
+                    do_continue = true;
+                    break;
+                }    
+            }
+
+            if (do_continue)
+                continue;
+
+            return true; //if this point is reached, the sequence was found
+        }
+
+        return false; // if no sequence is found - return false aka ID is valid;
+    }
+
+    private bool OneLongSeq(string id){
+        foreach (char i in id)
+        {
+            if (i != id[0])
+                return false;
+        }
+        return true;
     }
 
 }
