e3f3657812b6cd34d0c319cd056a1b854f87ac9c
Daxin18 <kamilciag123@gmail.com>
2025-12-03 13:53:42 +0100

    D3p2

--- code/D03/D3.cs
+++ code/D03/D3.cs
@@ -9,11 +9,19 @@ public class D3 : IDay
 {
     string[] banks = [];
     long total_output = 0;
+    bool use_recursion = true;
 
     public void Solve(){
         foreach (string bank in banks)
         {
-            total_output += GetMaxJoltage(bank);
+            if (use_recursion)
+            {
+                total_output += GetMaxJoltageRec(bank, 12);
+            }
+            else
+            {
+                total_output += GetMaxJoltage(bank);
+            }
         }
 
         Console.WriteLine("Solution: " + total_output);
@@ -65,5 +73,41 @@ public class D3 : IDay
         var result_string = bank[first_idx].ToString() + bank[second_idx].ToString();
         var result = Convert.ToInt32(result_string);
         return result;
+    }
+
+    //find biggest index, leaving space for the remaining iterations
+    // cut substring
+    // repeat
+    // NOTE: iterations_remaining INCLUDES CURRENT ITERATION
+    private long GetMaxJoltageRec(string bank, int iterations_remaining, string result = "")
+    {
+        var idx = 0;
+        var current_max = '0';
+
+        //finding highest number, leaving space for the rest
+        for (int i = 0; i < bank.Length - (iterations_remaining - 1); i++)
+        {
+            if (bank[i] > current_max)
+            {
+                current_max = bank[i];
+                idx = i;
+                if (bank[i] == '9') //instantly break on a 9 - the highest possible
+                    break;
+            }
+        }
+
+        iterations_remaining -= 1;
+        result += current_max.ToString();
+
+        if (iterations_remaining == 0) //exit condition
+        {
+            var return_value = Convert.ToInt64(result);
+            return return_value;
+        }
+        else 
+        {
+            var substring = bank.Substring(idx + 1);
+            return GetMaxJoltageRec(substring, iterations_remaining, result);
+        }
     }
 }
