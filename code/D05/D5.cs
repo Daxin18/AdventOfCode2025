88869d9ae2489d73ed2f51b0b9e8afc744377ca4
Daxin18 <kamilciag123@gmail.com>
2025-12-05 13:18:51 +0100

    D5p2

--- code/D05/D5.cs
+++ code/D05/D5.cs
@@ -9,18 +9,31 @@ public class D5 : IDay
 {
     List<(long from, long to)> ranges = new List<(long,long)>();
     List<long> available_ids = new List<long>();
+    bool do_puzzle_2 = true;
 
     public void Solve(){
-        int counter = 0;
+        long counter = 0;
 
-        foreach (long id in available_ids)
+        if(do_puzzle_2)
         {
-            foreach ((long from, long to) range in ranges)
+            var no_overlap = RemoveOverlap();
+
+            foreach ((long from, long to) range in no_overlap)
             {
-                if (id >= range.from && id <= range.to)
+                counter += range.to - range.from + 1;
+            }
+        }
+        else
+        {
+            foreach (long id in available_ids)
+            {
+                foreach ((long from, long to) range in ranges)
                 {
-                    counter++;
-                    break; //only breaks the inner loop
+                    if (id >= range.from && id <= range.to)
+                    {
+                        counter++;
+                        break; //only breaks the inner loop
+                    }
                 }
             }
         }
@@ -39,6 +52,54 @@ public class D5 : IDay
         idx++;
         ExtractIds(lines, idx);
     } 
+
+    private List<(long,long)> RemoveOverlap()
+    {
+        var result = new List<(long,long)>();
+        int i = 0;
+
+        //i is the index of the "current range"
+        //we then iterate over all the ranges after that, extending the current range if they overlap
+        //at the end of each iteration, "range" is the sum (merged range) of all ranges that overlap with the "current range"
+        //after that, the lowest range that did not overlap is set as the new "current range"
+        while (i < ranges.Count())
+        {
+            (long from, long to) range = ranges[i];
+            var starting_i = i;
+            var do_emergency_end = true; //just in case i == ranges.Count() - 1, it is impossible to enter the inner loop, so i won't change
+
+            //iterate over remaining ranges, merging those that overlap
+            for(int j = i + 1; j < ranges.Count(); j++)
+            {
+                do_emergency_end = false;
+                (long from, long to) next_range = ranges[j];
+
+                if (next_range.from <= range.to)
+                {
+                    if(range.to < next_range.to)
+                    {
+                        range.to = next_range.to;
+                    }
+                }
+                else
+                {
+                    i = j;
+                    break;
+                }
+
+                //only reachable if there was no range that did not overlap
+                i = ranges.Count();
+            }
+            result.Add(range);
+
+            if(do_emergency_end)
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
 
     private void ClearAll()
     {
@@ -59,6 +120,9 @@ public class D5 : IDay
 
             idx++;
         }
+
+        //sort the ranges so that the second puzzle is more doable;
+        ranges = ranges.OrderBy(r => r.from).ToList();
     }
 
     private void ExtractIds(string[] lines, int idx)
